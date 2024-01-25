8590cc56fa0b956458a39e1f2478921fe13fd78e
chienhuak <christy.kch.c@gmail.com>
2024-01-26 00:05:38 +0800

    separate data frame because of excel limitation (testH)

--- parsing-log.py
+++ parsing-log.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  9 16:03:10 2023
+Created on Wed Dec 13 15:26:22 2023
 
 @author: chienhuak
 """
@@ -10,11 +10,9 @@ import pandas as pd
 
 def list_files_and_directories_to_excel(path, output_excel_path):
     try:
-        data = {'目錄': [], '檔案': []}
-
         # 使用os.walk獲取目錄結構
+        data = {'目錄': [], '檔案': []}
         for root, dirs, files in os.walk(path):
-            # 列印目錄
             for file in files:
                 data['目錄'].append(root)
                 data['檔案'].append(file)
@@ -22,21 +20,29 @@ def list_files_and_directories_to_excel(path, output_excel_path):
         # 將數據轉換成DataFrame
         df = pd.DataFrame(data)
 
-        # 將DataFrame保存到Excel檔案
-        df.to_excel(output_excel_path, index=False)
+        # 拆分 DataFrame 為多個小的 DataFrame
+        chunk_size = 1048575  # Excel 表格的行數上限
+        chunks = [df[i:i + chunk_size] for i in range(0, df.shape[0], chunk_size)]
+
+        # 建立 Excel writer
+        with pd.ExcelWriter(output_excel_path, engine='xlsxwriter') as writer:
+            for idx, chunk in enumerate(chunks):
+                # 將每個小 DataFrame 寫入 Excel 工作表
+                chunk.to_excel(writer, sheet_name=f'Sheet{idx + 1}', index=False)
+
         print(f'已將檔案清單保存到 {output_excel_path}')
 
-        # 打開Excel檔案
+        # 打開 Excel 檔案
         os.system(output_excel_path)
 
     except Exception as e:
         print(f'發生錯誤: {e}')
 
 # 輸入你的路徑
-path = r'\\cgcfsii\Confidential\97_Test Result\Google\NBP90X\TTT'  # 請替換為你的實際路徑
+path = r'\\cgcfsii\Confidential\97_Test Result\Google'  # 請替換為你的實際路徑
 
-# 指定要保存的Excel檔案路徑
-output_excel_path = r'C:\Users\chienhuak\Desktop\testG.xlsx'  # 請替換為你想要保存的路徑
+# 指定要保存的 Excel 檔案路徑
+output_excel_path = r'C:\Users\chienhuak\Desktop\testH.xlsx'  # 請替換為你想要保存的路徑
 
-# 呼叫函數，將檔案清單保存到Excel檔案並打開
+# 呼叫函數，將檔案清單保存到 Excel 檔案並打開
 list_files_and_directories_to_excel(path, output_excel_path)
