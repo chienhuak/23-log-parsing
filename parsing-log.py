9cd52fba7a793445741ffa6c2d077b044a9fa8e1
chienhuak <christy.kch.c@gmail.com>
2024-01-29 00:04:45 +0800

    try to get file list inside zip folder (testK)

--- parsing-log.py
+++ parsing-log.py
@@ -1,43 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Dec 14 11:29:32 2023
+Created on Mon Dec 18 11:26:11 2023
 
 @author: chienhuak
 """
 
 import os
 import pandas as pd
-
-
-def extract_folder_name(path):
-    # 尋找 "TT\" 的位置
-    start_index = path.find('TT\\')
-    
-    # 如果找到 "TT\"，則在這個位置後尋找下一個反斜線 "\"
-    if start_index != -1:
-        end_index = path.find('\\', start_index + 3)
-        if end_index != -1:
-            return path[start_index + 3:end_index]
-    
-    return ''  # 如果找不到匹配，返回空字符串
-
-def count_backslashes(path):
-    # 計算路徑中反斜線的數量
-    return path.count('\\')
+import zipfile
 
 def list_files_and_directories_to_csv(path, output_csv_path):
     try:
         # 使用os.walk獲取目錄結構
-        data = {'目錄': [], '檔案名稱': [], '副檔名': [], '擷取的資訊': [], '反斜線數量': []}
+        data = {'目錄': [], '檔案名稱': [], '副檔名': [], '反斜線數量': []}
         for root, dirs, files in os.walk(path):
             for file in files:
                 data['目錄'].append(root)
                 data['檔案名稱'].append(os.path.splitext(file)[0])  # 取得檔案名稱
                 data['副檔名'].append(os.path.splitext(file)[1])  # 取得副檔名
-                data['擷取的資訊'].append(extract_folder_name(root))  # 擷取資訊
-                data['反斜線數量'].append(count_backslashes(root))  # 計算反斜線數量
+                data['反斜線數量'].append(root.count('\\'))  # 計算反斜線數量
 
-        # 將數據轉換成DataFrame
+        # 檢查是否為 zip 檔案，如果是，則加入 zip 內的檔案
+        if path.endswith('.zip'):
+            with zipfile.ZipFile(path, 'r') as zip_ref:
+                for file_info in zip_ref.infolist():
+                    file_path = file_info.filename
+                    data['目錄'].append(os.path.dirname(file_path))
+                    data['檔案名稱'].append(os.path.splitext(os.path.basename(file_path))[0])
+                    data['副檔名'].append(os.path.splitext(os.path.basename(file_path))[1])
+                    data['反斜線數量'].append(file_path.count('/'))  # 計算反斜線數量（zip 內使用 /）
+
+        # 將數據轉換成 DataFrame
         df = pd.DataFrame(data)
 
         # 將 DataFrame 寫入 CSV 檔案
@@ -52,7 +45,7 @@ def list_files_and_directories_to_csv(path, output_csv_path):
 path = r'\\cgcfsii\Confidential\97_Test Result\Google\NBPB00\TTT'  # 請替換為你的實際路徑
 
 # 指定要保存的 CSV 檔案路徑
-output_csv_path = r'C:\Users\chienhuak\Desktop\testJ.csv'  # 請替換為你想要保存的路徑
+output_csv_path = r'C:\Users\chienhuak\Desktop\testK_zip.csv'  # 請替換為你想要保存的路徑
 
 # 呼叫函數，將檔案清單保存到 CSV 檔案
 list_files_and_directories_to_csv(path, output_csv_path)
