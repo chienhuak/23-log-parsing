666cad6cf24663032e621225a92cc25e0604d80f
chienhuak <christy.kch.c@gmail.com>
2024-02-05 23:06:48 +0800

    Add Keysight TP168 "TCReport" rule (testN)

--- parsing-log.py
+++ parsing-log.py
@@ -26,6 +26,44 @@ def extract_folder_name(path):
     
     return ''  # 如果找不到匹配，返回空字符串
 
+def extract_info_from_168html(file_path):
+    try:
+        with open(file_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+            soup = BeautifulSoup(content, 'html.parser')
+
+            # 尋找 Test case ID
+            test_case_id_tag = soup.find('th', {'scope': 'col'}, text='Test Case Number and Name:')
+            test_case_id = test_case_id_tag.find_next('td').text.split('|')[-1].strip() if test_case_id_tag else ''
+#            test_case_id = test_case_id_tag.find_next('td').text.strip() if test_case_id_tag else ''
+
+
+            # 尋找 Band
+            band_tag = soup.select_one('#divTestSummary table.summary-table tbody tr.summary-tr-body.border-bottom td:nth-child(6)')
+            band = band_tag.text.strip() if band_tag else ''
+
+
+            # 尋找 Configuration
+            configuration_tag = soup.select_one('#divTestSummary table.summary-table tbody tr.summary-tr-body.border-bottom td:nth-child(5)')
+            configuration = configuration_tag.text.strip() if band_tag else ''
+
+            # 尋找所有符合條件的 tr 元素
+            # rows = soup.find_all('tr', class_='summary-tr-body border-bottom')
+
+
+#            configuration_tag = soup.find('th', {'scope': 'col'}, text='Test Case Title:')
+#            configuration = configuration_tag.find_next('td').text.strip() if configuration_tag else ''
+
+            # 尋找 Result
+            result_tag = soup.find('th', {'scope': 'col'}, text='Test Case Verdict:')
+            result = result_tag.find_next('td').text.strip() if result_tag else ''
+
+            return test_case_id, band, configuration, result
+
+    except Exception as e:
+        print(f'解析 HTML 時發生錯誤: {e}')
+        return '', '', '', ''
+
 def extract_info_from_html(file_path):
     try:
         with open(file_path, 'r', encoding='utf-8') as file:
@@ -59,6 +97,24 @@ def list_files_and_directories_to_csv(path, output_csv_path):
         # 使用os.walk獲取目錄結構
         data = {'Path': [], 'File Name': [], 'File Types': [], 'TP': [],'Test case ID': [], 'Band': [], 'Configuration': [], 'Result': [], 'Count /': []}
         for root, dirs, files in os.walk(path):
+            # 過濾檔案名稱包含 'TCReport' 的檔案
+            tc_report_files = [file for file in files if 'TCReport' in file]
+            for file in tc_report_files:
+                file_path = os.path.join(root, file)
+                data['Path'].append(root)
+                data['File Name'].append(os.path.splitext(file)[0])  # 取得檔案名稱
+                data['File Types'].append(os.path.splitext(file)[1])  # 取得副檔名
+                data['TP'].append(extract_folder_name(root))  # 擷取資訊
+
+                # 擷取 HTML 中的資訊
+                test_case_id, band, configuration, result = extract_info_from_168html(file_path)
+
+                data['Test case ID'].append(test_case_id)
+                data['Band'].append(band)
+                data['Configuration'].append(configuration)
+                data['Result'].append(result)
+                data['Count /'].append(count_backslashes(root))  # 計算反斜線數量
+
             # 過濾檔案名稱為'OnlineReport'的檔案
             online_report_files = [file for file in files if file.lower() == 'onlinereport.htm']
             for file in online_report_files:
@@ -90,10 +146,10 @@ def list_files_and_directories_to_csv(path, output_csv_path):
 
 
 # 輸入你的路徑
-path = r'\\cgcfsii\Confidential\97_Test Result\Google\NBPB00\TTT\298(b)\20230318'  # 請替換為你的實際路徑
+path = r'C:\Users\chienhuak\Downloads\Log download\TK4\NBPF02\298(b)_part2'  # 請替換為你的實際路徑
 
 # 按需指定要保存的 CSV 檔案路徑
-output_csv_path = r'C:\Users\chienhuak\Desktop\testM.csv'  # 請替換為你想要保存的路徑
+output_csv_path = r'C:\Users\chienhuak\Desktop\testN.csv'  # 請替換為你想要保存的路徑
 
 # 呼叫函數，將檔案清單保存到 CSV 檔案
 list_files_and_directories_to_csv(path, output_csv_path)
